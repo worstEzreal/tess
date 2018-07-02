cbf7fd59f76c211b2ae78a6012605ac2e46c28a9
zengxzh <378741705@qq.com>
2018-07-02 10:34:27 +0800

    1st commit

---
+++ src/main/java/com/worstEzreal/tess/TessApplication.java
@@ -0,0 +1,12 @@
+package com.worstEzreal.tess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TessApplication.class, args);
+	}
+}
---
+++ src/test/java/com/worstEzreal/tess/TessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.worstEzreal.tess;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
